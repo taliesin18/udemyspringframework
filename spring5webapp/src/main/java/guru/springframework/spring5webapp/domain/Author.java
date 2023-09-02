c413cba8d765d1db1b16bd113665e3689c64ba42
ncabiliza <nblcabiliza@gmail.com>
2023-09-02 12:19:08 +0800

    Added CrudRepository

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-
+import java.util.HashSet;
 @Entity
 public class Author {
 	
@@ -20,16 +20,15 @@ public class Author {
 	private String lastName; 
 	
 	@ManyToMany(mappedBy = "authors")
-	private Set<Book> books;
+	private Set<Book> books = new HashSet<>();
 
 	public Author() {
 
 	}
 
-	public Author(String firstName, String lastName, Set<Book> books) {
+	public Author(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.books = books;
 	}
 
 	public Long getId() {
--- spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp.domain;
 
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -24,13 +25,12 @@ public class Book {
 	@ManyToMany
 	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
 	inverseJoinColumns = @JoinColumn(name = "author_id"))
-	private Set<Author> authors;
+	private Set<Author> authors  = new HashSet<>();
 
-	public Book(String title, String isbn, Set<Author> authors) {
+	public Book(String title, String isbn) {
 		super();
 		this.title = title;
 		this.isbn = isbn;
-		this.authors = authors;
 	}
 	
 	
